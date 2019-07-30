a4c934e0d55749cbd98f621173bdf5af5521cdd7
meettyj <meettianyijun@gmail.com>
2019-07-30 16:06:03 -0400

    update for bk research cluster

---
+++ bkResearchCluster/unzipFilesInDirectories_2019.sh
@@ -0,0 +1,16 @@
+cd juliana/
+
+for dir in */
+do
+    dir=${dir%*/}
+    # echo ${dir##*/}
+    for file in $dir/*
+    do
+	    if [[ ${file:0:4} -eq "2019" ]]
+	    then
+	    # echo sub is 2018 directory ${file}
+	        # echo $file
+	        tar -zxvf $file
+	    fi
+    done
+done
